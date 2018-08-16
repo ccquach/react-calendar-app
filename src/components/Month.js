44769662da74f3f0db65c05a3b7ef9ab1d85148d
cynthia.quach <ccquach93@gmail.com>
2018-08-15 22:54:57 -0700

    fix january weeks failure to render, with firstWeek reset on last week of year

--- src/components/Month.js
+++ src/components/Month.js
@@ -3,27 +3,27 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import moment from 'moment';
 import Week from './Week';
+import { getDaysInMonth } from '../helpers/getDaysInMonth';
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
 `;
 
-function getDaysInMonth(month, year) {
-  var date = new Date(year, month, 1);
-  var days = [];
-  while (date.getMonth() === month) {
-    days.push(new Date(date));
-    date.setDate(date.getDate() + 1);
-  }
-  return days;
-}
+const getDaysOfWeek = (days, firstWeek, i) =>
+  days.filter(
+    day =>
+      moment(day)
+        .startOf('week')
+        .isoWeek() ===
+      firstWeek + i
+  );
 
 const Month = ({ month, year }) => {
   // get array of days for the month
   const days = getDaysInMonth(month, year);
   // get week index of first week of the month
-  const firstWeek = moment(days[0])
+  let firstWeek = moment(days[0])
     .startOf('week')
     .isoWeek();
 
@@ -32,18 +32,14 @@ const Month = ({ month, year }) => {
   // check for days data five weeks after first week of month
   // max possible number of weeks in a month is six
   for (let i = 0; i < 6; i++) {
-    const daysOfWeek = days.filter(
-      day =>
-        moment(day)
-          .startOf('week')
-          .isoWeek() ===
-        firstWeek + i
-    );
+    const daysOfWeek = getDaysOfWeek(days, firstWeek, i);
     // if day data exists for week index, create Week component
     if (!!daysOfWeek.length)
       weeks.push(
         <Week key={`week-${i}`} index={firstWeek + i} days={daysOfWeek} />
       );
+    // restart firstWeek at 1 if previous value is 52
+    if (firstWeek === 52) firstWeek = 0;
   }
   // render Week components
   return <Wrapper>{weeks}</Wrapper>;
---
+++ src/helpers/getDaysInMonth.js
@@ -0,0 +1,9 @@
+export function getDaysInMonth(month, year) {
+  var date = new Date(year, month, 1);
+  var days = [];
+  while (date.getMonth() === month) {
+    days.push(new Date(date));
+    date.setDate(date.getDate() + 1);
+  }
+  return days;
+}
